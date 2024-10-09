ec19feab5ffed9ea6d13dc211bd19d0a513f6bb0
Corey Steinwand <corey.steinwand@gmail.com>
2024-10-09 18:23:56 -0500

    test

---
+++ components/Footer.vue
@@ -0,0 +1,168 @@
+<template>
+  <div class="bg-dark-900">
+    <footer class="p-10 px-2 mx-auto footer-custom max-w-7xl sm:px-6 lg:px-8">
+
+      <!-- Main Footer Content -->
+      <div class="grid grid-cols-2 gap-8 md:grid-cols-5 text-light">
+        <!-- Legal Section -->
+        <nav class="mb-6">
+          <h2 class="footer-title">Legal</h2>
+          <NuxtLink to="/privacy-policy" class="flex items-center mb-2 link link-hover text-secondary-100">
+            <client-only>
+              <i class="mr-2 fal fa-shield-alt"></i>
+            </client-only> Privacy Policy
+          </NuxtLink>
+          <NuxtLink to="/terms-of-service" class="flex items-center mb-2 link link-hover text-secondary-100">
+            <client-only>
+              <i class="mr-2 fal fa-file-contract"></i>
+            </client-only> Terms of Service
+          </NuxtLink>
+          <NuxtLink to="/disclaimer" class="flex items-center mb-2 link link-hover text-secondary-100">
+            <client-only>
+              <i class="mr-2 fal fa-exclamation-triangle"></i>
+            </client-only> Disclaimer
+          </NuxtLink>
+        </nav>
+
+        <!-- Social Section -->
+        <nav class="mb-6">
+          <h2 class="footer-title">Social</h2>
+          <div class="flex space-x-4">
+            <a href="https://twitter.com/GrowWithParker" target="_blank" rel="noopener noreferrer" aria-label="Twitter (opens in a new tab)">
+              <client-only>
+                <i class="text-2xl fab fa-twitter"></i>
+              </client-only>
+            </a>
+            <a href="https://youtube.com/@parker_blunt_weed_garden?si=GhZQu5Ti6-f5IJl9" target="_blank" rel="noopener noreferrer" aria-label="YouTube (opens in a new tab)">
+              <client-only>
+                <i class="text-2xl fab fa-youtube"></i>
+              </client-only>
+            </a>
+            <a href="https://www.facebook.com/profile.php?id=61566402899772" target="_blank" rel="noopener noreferrer" aria-label="Facebook (opens in a new tab)">
+              <client-only>
+                <i class="text-2xl fab fa-facebook"></i>
+              </client-only>
+            </a>
+          </div>
+        </nav>
+
+        <nav class="mb-6">
+          <h2 class="footer-title">Links</h2>
+
+          <!-- Blog Link -->
+          <a href="https://ghost.weed-garden.com" rel="noopener noreferrer" target="_blank" class="flex items-center mb-2 link link-hover text-secondary-100">
+            <client-only>
+              <i class="mr-2 fas fa-rss"></i>
+            </client-only> Blog
+          </a>
+
+          <!-- Help Form Link -->
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLScNNqa-RveU13qoEZZiqx7QSI9DHq-fuFpOTvboBwZZnBbJ8g/viewform?usp=sf_link" rel="noopener noreferrer" target="_blank" class="flex items-center mb-2 link link-hover text-secondary-100">
+            <client-only>
+              <i class="mr-2 fas fa-life-ring"></i>
+            </client-only> Help Form
+          </a>
+
+          <!-- Discord Link -->
+          <a href="https://discord.gg/T3ywCHSR" rel="noopener noreferrer" target="_blank" class="flex items-center mb-2 link link-hover text-secondary-100">
+            <client-only>
+              <i class="mr-2 fab fa-discord"></i>
+            </client-only> Discord
+          </a>
+
+          <!-- GitHub Link -->
+          <a href="https://github.com/cs-weed-garden" rel="noopener noreferrer" target="_blank" class="flex items-center mb-2 link link-hover text-secondary-100">
+            <client-only>
+              <i class="mr-2 fab fa-github"></i>
+            </client-only> GitHub Org
+          </a>
+        </nav>
+
+        <!-- Resources Section -->
+        <nav class="mb-6">
+          <h2 class="footer-title">Resources</h2>
+
+          <!-- Sitemap -->
+          <a href="/sitemap_index.xml" rel="noopener noreferrer" class="flex items-center mb-2 link link-hover text-secondary-100">
+            <client-only>
+              <i class="mr-2 fal fa-sitemap"></i>
+            </client-only> Sitemap
+          </a>
+
+          <!-- Robots.txt -->
+          <a href="/robots.txt" rel="noopener noreferrer" class="flex items-center mb-2 link link-hover text-secondary-100">
+            <client-only>
+              <i class="mr-2 fal fa-robot"></i>
+            </client-only> Robots.txt
+          </a>
+
+          <!-- Humans.txt (Credits) -->
+          <a href="/Humans.txt" class="flex items-center mb-2 link link-hover text-secondary-100">
+            <client-only>
+              <i class="mr-2 fal fa-users"></i>
+            </client-only> Humans.txt (Credits)
+          </a>
+
+          <!-- Accessibility Statement -->
+          <NuxtLink to="/accessibility" class="flex items-center mb-2 link link-hover text-secondary-100">
+            <client-only>
+              <i class="mr-2 fal fa-universal-access"></i>
+            </client-only> Accessibility Statement
+          </NuxtLink>
+        </nav>
+
+        <!-- Contact Information Section -->
+        <nav class="mb-6">
+          <h2 class="footer-title">Contact Information</h2>
+          <address class="space-y-2 text-sm not-italic leading-6 text-secondary-100">
+            <p class="flex items-center font-semibold">
+              <client-only>
+                <i class="mr-2 fal fa-building text-secondary-200"></i>
+              </client-only> CS Industries, LLC.
+            </p>
+            <p class="flex items-center">
+              <client-only>
+                <i class="mr-2 fal fa-map-marker-alt text-secondary-200"></i>
+              </client-only> Hopkins, MN 55343
+            </p>
+            <p class="flex items-center">
+              <client-only>
+                <i class="mr-2 fal fa-phone text-secondary-200"></i>
+              </client-only>
+              <a href="tel:+15072912592" class="link link-hover text-secondary-100 hover:text-secondary-300"> (507) 291-2592</a>
+            </p>
+            <p class="flex items-center">
+              <client-only>
+                <i class="mr-2 fal fa-envelope text-secondary-200"></i>
+              </client-only>
+              <a href="mailto:info@weed-garden.com" class="link link-hover text-secondary-100 hover:text-secondary-300">info@weed-garden.com</a>
+            </p>
+          </address>
+        </nav>
+      </div>
+
+      <div class="flex items-center text-secondary-100">
+        <!-- Icon Section -->
+        <div class="text-4xl">
+          <client-only>
+            <i class="fal fa-hashtag"></i>
+          </client-only>
+        </div>
+
+        <!-- Text Section -->
+        <div class="ml-4">
+          <p>Protected by Cloudflare & TLS 1.3</p>
+          <p>Serving cannabis growers throughout Minnesota.</p>
+        </div>
+      </div>
+
+      <div class="mt-8 text-secondary-200">
+        <p>&copy; 2024 CS Industries, LLC. All rights reserved. <br>
+          Doing business as <strong>Weed Garden</strong>.</p>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ components/Navigation.vue
@@ -0,0 +1,70 @@
+<template>
+    <nav class="bg-dark-900">
+      <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div class="flex items-center justify-between h-16">
+          <div class="flex items-center">
+            <img class="w-auto h-8" src="/site-logo.webp" alt="Weed Garden Icon">
+            <span class="ml-2 text-xl font-bold text-white">Weed Garden</span>
+          </div>
+  
+          <div class="sm:block">
+            <div class="flex items-baseline ml-10 space-x-4">
+              <NuxtLink to="/" class="px-3 py-2 text-sm font-medium text-white rounded-md hover:bg-gray-700">Home</NuxtLink>
+              <NuxtLink to="/video-tour" class="px-3 py-2 text-sm font-medium text-white rounded-md hover:bg-gray-700">Video Tour</NuxtLink>
+              <NuxtLink to="/features" class="px-3 py-2 text-sm font-medium text-white rounded-md hover:bg-gray-700">Features</NuxtLink>
+              <NuxtLink to="/mission" class="px-3 py-2 text-sm font-medium text-white rounded-md hover:bg-gray-700">Mission</NuxtLink>
+              <NuxtLink to="/about-us" class="px-3 py-2 text-sm font-medium text-white rounded-md hover:bg-gray-700">About Us</NuxtLink>
+            </div>
+          </div>
+  
+          <div class="flex items-center">
+           <NuxtLink external href="https://app.weed-garden.com/login" class="px-4 py-2 ml-3 text-sm font-medium text-white rounded-md bg-primary hover:bg-primary-light">Login</NuxtLink>
+          </div>
+  
+          <div class="flex sm:hidden">
+            <button type="button" class="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" @click="toggleMobileMenu">
+              <span class="sr-only">Open main menu</span>
+              <svg class="w-6 h-6" v-if="!mobileMenuOpen" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
+              </svg>
+              <svg class="w-6 h-6" v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+  
+      <!-- Mobile Menu -->
+      <div v-if="mobileMenuOpen" class="sm:hidden" id="mobile-menu">
+        <div class="px-2 pt-2 pb-3 space-y-1">
+          <NuxtLink to="/" class="block px-3 py-2 text-base font-medium text-white rounded-md hover:bg-gray-700">Home</NuxtLink>
+          <NuxtLink to="/cannabis-erp-software" class="block px-3 py-2 text-base font-medium text-white rounded-md hover:bg-gray-700">ERP Software</NuxtLink>
+          <NuxtLink to="/features" class="block px-3 py-2 text-base font-medium text-white rounded-md hover:bg-gray-700">Features</NuxtLink>
+          <NuxtLink to="/pricing" class="block px-3 py-2 text-base font-medium text-white rounded-md hover:bg-gray-700">Pricing</NuxtLink>
+          <NuxtLink to="/blog" class="block px-3 py-2 text-base font-medium text-white rounded-md hover:bg-gray-700">Blog</NuxtLink>
+          <NuxtLink to="/contact" class="block px-3 py-2 text-base font-medium text-white rounded-md hover:bg-gray-700">Contact</NuxtLink>
+        </div>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        mobileMenuOpen: false,
+      };
+    },
+    methods: {
+      toggleMobileMenu() {
+        this.mobileMenuOpen = !this.mobileMenuOpen;
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Scoped styles here if needed */
+  </style>
+  
